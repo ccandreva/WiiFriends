73ad07b58022bcb5e7cd62a99724830f49d0623d
chris <chris@westnet.com>
2012-05-20 11:14:01 -0400

    Fix version number format.

--- lib/WiiFriends/Version.php
+++ lib/WiiFriends/Version.php
@@ -18,7 +18,7 @@ class WiiFriends_Version extends Zikula_AbstractVersion
         $meta['displayname']  = $this->__('Wii Friends');
         $meta['description']  = $this->__('Manage your Wii friends and console codes.',$dom);
         $meta['url']         = $this->__('wiifriends');
-        $meta['version']      = '1.1';
+        $meta['version']      = '1.1.1';
         $meta['core_min']      =   '1.3.0';
         $meta['core_max']      =   '1.3.99';
         $meta['official']     = true;
